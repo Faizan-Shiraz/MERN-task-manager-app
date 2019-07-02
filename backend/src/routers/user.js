631bceb005062414f3f928ff0e60e8d56fa0e358
Faizan-Shiraz <faizan.shiraz@arbisoft.com>
2019-07-02 14:57:30 +0500

    profile picture support added to backend

--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const multer = require("multer");
+const sharp = require("sharp");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
 
 const router = new express.Router();
+
+const upload = multer({
+  limits: {
+    fileSize: 10000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/.(jpg|png|jpeg)/))
+      return cb(new Error("File not supported nigga"));
+    cb(undefined, true);
+  }
+});
 
 router.post("/addNewUser", async (req, res) => {
   const user = new User(req.body);
@@ -84,6 +97,45 @@ router.post("/login", async (req, res) => {
   } catch (e) {
     res.status(400).send(e);
   }
+});
+
+router.get("/profile/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
+  } catch (e) {
+    res.status(404).send();
+  }
+});
+
+router.post(
+  "/profile/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    const buffer = await sharp(req.file.buffer)
+      .resize(250)
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+router.delete("/profile/avatar", auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.send();
 });
 
 module.exports = router;
